1f40ed0001d2c7c21cee54b3b60c311865fcb57f
naieem-bd <naieemur@gmail.com>
2021-02-24 18:21:17 +0600

    views added

--- controllers.js
+++ controllers.js
@@ -3,7 +3,7 @@ const Contact = require('./Contact')
 exports.getAllContact = (req, res) => {
     Contact.find()
         .then( contacts => {
-            res.json(contacts)
+            res.render('index', {contacts})
         })
         .catch( e => {
             console.log(e)
